4300880d643725e889811e2a17be14f087a3ebfa
titustum <tituskiptanuitum@gmail.com>
2024-06-17 20:47:32 +0300

    some updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const righteous= Righteous({ weight: '400', subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
-      <div className="py-10 flex-grow">
+      <div className="py-10 flex-grow w-[90%] mx-auto">
             <h1 className={`text-orange-600 uppercase text-2xl text-center font-bold ${righteous.className}`}>School Bus Routing System</h1>
 
             <p className="mt-5 text-zinc-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt autem obcaecati consequatur tempore laboriosam possimus necessitatibus ipsam voluptatum, fugiat saepe perspiciatis odio quas quos aperiam sapiente iure beatae molestiae voluptatem.</p>
@@ -15,7 +15,7 @@ export default function Home() {
             className="mx-auto w-70%"/>
             
         </div>
-        <div className="shrink-0 grid gap-2 mb-4">
+        <div className="shrink-0 grid gap-2 mb-8">
             <a href="https://schoolbusapp.webesi.co.ke/login" className="w-[90%] block text-center mx-auto py-3 px-6 bg-black rounded-md text-white hover:opacity-80">
                 Login
             </a>
